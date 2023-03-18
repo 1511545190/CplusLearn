f994e51774e3c5ae3db6314ff89d2a598bb4e3d9
1511545190@qq.com <1511545190@qq.com>
2023-03-18 22:13:19 +0800

    1_9_0:文件操作

---
+++ DAC/leetcode/leetcode.h
@@ -0,0 +1,46 @@
+//
+// Created by 刘日亮 on 2023/3/18.
+//
+
+#ifndef DAC_LEETCODE_H
+#define DAC_LEETCODE_H
+
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+     TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //DAC_LEETCODE_H
